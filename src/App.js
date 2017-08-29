09525ab3482646ddac3e3183f912cca640db853b
kidman90 <kacprzak90@gmail.com>
2017-08-29 14:25:21 +0200

    09 Normalize Events with Reacts Synthetic Event System

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    return <Title text="The Text" />;
+  constructor() {
+    super();
+    this.state = { currentEvent: '---' }
+    this.update = this.update.bind(this);
   }
-};
 
-const Title = (props) => <h1>Title: {props.text}</h1>;
+  update(e) {
+    this.setState({ currentEvent: e.type });
+  }
 
-Title.propTypes = {
-  // text: React.PropTypes.string.isRequired
-  text(props, propName, component) {
-    if (!(propName in props)) {
-      return new Error(`missing ${propName}`);
-    }
-    if (props[propName].length < 6) {
-      return new Error(`${propName} was too short`);
-    }
+  render() {
+    return (
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+          cols="30" 
+          rows="10" />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    );
   }
 };
 

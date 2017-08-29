50a08b36a9dc59945edcc46da2ee7728e7cb984f
kidman90 <kacprzak90@gmail.com>
2017-08-29 14:39:57 +0200

    16 Build a JSX Live Compiler as a React Component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
 import React from 'react';
-
-const HOC = (InnerComponent) => class extends React.Component {
-  constructor() {
-    super();
-    this.state = { count: 0 };
-  }
-
-  update() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  componentWillMount() {
-    console.log('will mount');
-  }
-
-  render() {
-    // console.log(this);
-    return (
-      <InnerComponent
-        {...this.props}
-        {...this.state}
-        update={this.update.bind(this)}
-      />
-    );
-  }
-}
+import './App.css';
 
 class App extends React.Component {
-  componentWillMount() {
-    console.log('label will mount');
+  constructor() {
+    super();
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    };
+  }
+
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+          .transform(code, { presets: ['es2015', 'react'] })
+          .code,
+        err: ''
+      });
+    } catch (err) {
+      this.setState({ err: err.message });
+    }
   }
 
   render() {
     return (
       <div>
-        <Button>button</Button>
-        <hr />
-        <LabelHOC>label</LabelHOC>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input} />
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
       </div>
     );
   }
 };
-
-const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>);
-
-class Label extends React.Component {
-  render() {
-    return (
-      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
-    );
-  }
-};
-
-const LabelHOC = HOC(Label);
 
 export default App;

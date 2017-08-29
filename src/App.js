d2c4f2539a1cee939fc87f7ef8f60f60f2a8fc8f
kidman90 <kacprzak90@gmail.com>
2017-08-29 14:35:24 +0200

    14 Use map to Create React Components from Arrays of Data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { increasing: false }
+    this.state = { items: [] };
   }
 
-  update() {
-    ReactDom.render(<App val={this.props.val + 1} />, document.getElementById('root'));
+  componentWillMount() {
+    fetch('http://swapi.co/api/people/?format=json')
+      .then(response => response.json())
+      .then(({ results: items }) => this.setState({ items }));
   }
 
-  componentWillReceiveProps(nextProps) {
-    // console.log(nextProps);
-    this.setState({ increasing: nextProps.val > this.props.val });
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
+  filter(e) {
+    this.setState({ filter: e.target.value });
   }
 
   render() {
-    console.log(this.state.increasing);
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter(item =>
+        item.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+    }
     return (
-      <button onClick={this.update.bind(this)}>
-        {this.props.val}
-      </button>
+      <div>
+        <input type="text"
+          onChange={this.filter.bind(this)} />
+        {items.map(item => 
+          <Person key={item.name} person={item} />)}
+      </div>
     );
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`);
-  }
-
 };
 
-App.defaultProps = { val: 0 };
+const Person = (props) => <h4>{props.person.name}</h4>;
 
 export default App;

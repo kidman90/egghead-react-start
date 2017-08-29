1d57be6f15d689d0fcf4fe1ba3fdff9e7eeb7084
kidman90 <kacprzak90@gmail.com>
2017-08-29 14:36:43 +0200

    15 Compose React Component Behavior with Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import React from 'react';
 
-class App extends React.Component {
+const HOC = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
-    this.state = { items: [] };
+    this.state = { count: 0 };
+  }
+
+  update() {
+    this.setState({ count: this.state.count + 1 });
   }
 
   componentWillMount() {
-    fetch('http://swapi.co/api/people/?format=json')
-      .then(response => response.json())
-      .then(({ results: items }) => this.setState({ items }));
-  }
-
-  filter(e) {
-    this.setState({ filter: e.target.value });
+    console.log('will mount');
   }
 
   render() {
-    let items = this.state.items;
-    if (this.state.filter) {
-      items = items.filter(item =>
-        item.name.toLowerCase().includes(this.state.filter.toLowerCase()));
-    }
+    // console.log(this);
+    return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    );
+  }
+}
+
+class App extends React.Component {
+  componentWillMount() {
+    console.log('label will mount');
+  }
+
+  render() {
     return (
       <div>
-        <input type="text"
-          onChange={this.filter.bind(this)} />
-        {items.map(item => 
-          <Person key={item.name} person={item} />)}
+        <Button>button</Button>
+        <hr />
+        <LabelHOC>label</LabelHOC>
       </div>
     );
   }
 };
 
-const Person = (props) => <h4>{props.person.name}</h4>;
+const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>);
+
+class Label extends React.Component {
+  render() {
+    return (
+      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
+    );
+  }
+};
+
+const LabelHOC = HOC(Label);
 
 export default App;

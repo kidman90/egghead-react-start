d2028400ac3292dd26ff3a0032c67d029bfd9312
kidman90 <kacprzak90@gmail.com>
2017-08-29 14:29:36 +0200

    11 Understand the React Component Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { a: '' }
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
   }
 
   update() {
-    this.setState({
-      a: this.a.refs.input.value,
-      b: this.refs.b.value
-    });
+    this.setState({ val: this.state.val + 1 });
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  render() {
+    console.log('render');
+    return <button onClick={this.update}>{this.state.val}</button>;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+};
+
+class Wrapper extends React.Component {
+  mount() {
+    ReactDom.render(<App />, document.getElementById('a'));
+  }
+
+  unmount() {
+    ReactDom.unmountComponentAtNode(document.getElementById('a'));
   }
 
   render() {
     return (
       <div>
-        <Input
-          ref={component => this.a = component}
-          update={this.update.bind(this)}
-        /> {this.state.a}
-        <hr />
-        <input
-          ref="b"
-          type="text"
-          onChange={this.update.bind(this)}
-        /> {this.state.b}
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmoint</button>
+        <div id="a"></div>
       </div>
     );
   }
 };
 
-class Input extends React.Component {
-  render() {
-    return <div><input ref="input" type="text" onChange={this.props.update} /></div>;
-  }
-};
-
-export default App;
+export default Wrapper;

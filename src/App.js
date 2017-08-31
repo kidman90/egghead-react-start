b117977c610ea6f32d8c59c2d09bd8df6ec603ba
kidman90 <kacprzak90@gmail.com>
2017-08-31 16:57:21 +0200

    20 Write More Reusable React Components with Composable APIs

--- src/App.js
+++ src/App.js
@@ -1,40 +1,74 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      red: 0
+    };
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+    });
+  }
+
   render() {
     return (
-      <Buttons>
-        <button value="A">A</button>
-        <button value="B">B</button>
-        <button value="C">C</button>
-      </Buttons>
+      <div>
+        <NumInput
+          ref="red"
+          min={0}
+          max={255}
+          step={0.01}
+          val={+this.state.red}
+          type="number"
+          label="Red"
+          update={this.update} />
+      </div>
     );
   }
 };
 
-class Buttons extends React.Component {
-  constructor() {
-    super();
-    this.state = { selected: 'None' };
-  }
-
-  selectedItem(selected) {
-    this.setState({ selected });
-  }
-
+class NumInput extends React.Component {
   render() {
-    let fn = child =>
-      React.cloneElement(child, {
-        onClick: this.selectedItem.bind(this, child.props.value)
-      });
-    let items = React.Children.map(this.props.children, fn);
+    let label = this.props.label !== '' ? <label>{this.props.label} - {this.props.val}</label> : '';
     return (
       <div>
-        <h2>You have Selected: {this.state.selected}</h2>
-        {items}
+        <input
+          ref="inp"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update} />
+        {label}
       </div>
     );
   }
+};
+
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number', 'range'])
+};
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
 };
 
 export default App;

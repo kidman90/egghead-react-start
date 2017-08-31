6b5f4b3ad9c1be04d076f9491c38725741d16831
kidman90 <kacprzak90@gmail.com>
2017-08-31 16:13:47 +0200

    19 Use React.cloneElement to Extend Functionality of Children Components

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <Parent>
-        <div className="childA"></div>
-        {/* <div className="childB"></div> */}
-      </Parent>
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
     );
   }
 };
 
-class Parent extends React.Component {
+class Buttons extends React.Component {
+  constructor() {
+    super();
+    this.state = { selected: 'None' };
+  }
+
+  selectedItem(selected) {
+    this.setState({ selected });
+  }
+
   render() {
-    // console.log(this.props.children);
-
-    // let items = React.Children
-    //   .map(this.props.children, child => child);
-
-    // let items = React.Children.toArray(this.props.children);
-
-    let items = React.Children.only(this.props.children);
-
-    console.log(items);
-
-    React.Children.forEach(this.props.children, child => console.log(child.props.className));
-
-    return null;
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick: this.selectedItem.bind(this, child.props.value)
+      });
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have Selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    );
   }
 };
 
